12a703fbfc090f53c20cef69eb3c4830be88d3e0
nicolas <kar0lin3>
2017-06-16 11:24:47 +0200

    - fix parameter name

--- Controller/TicketController.php
+++ Controller/TicketController.php
@@ -36,7 +36,7 @@ class TicketController extends Controller
         $ticketFeature = $this->get('hackzilla_ticket.features');
 
         //use at your own risks, cron to the command is better, todo: move this to a special action to force mail retrieving
-        if (true === $this->container->getParameter('hackzilla_ticket.check_email_from_controller')) {
+        if (true === $this->container->getParameter('hackzilla_ticket.from_mail.check_email_from_controller')) {
             //it's better to refactor console command to not use console command in controller, but to bvoid code duplication, better way should be to create a service, and use it both in here and the console command ?
             if ($ticketFeature->hasFeature('from_mail')) {
                 $kernel = $this->get('kernel');
